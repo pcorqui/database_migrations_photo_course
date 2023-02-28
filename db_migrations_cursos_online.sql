ffbfad99de19cacc232d79bd18757373ef42ca4b
Paul Cortés Quijano <paul.cortes@forteinnovation.mx>
2023-02-27 23:42:39 -0600

    fix errors sintanxis

--- db_migrations_cursos_online.sql
+++ db_migrations_cursos_online.sql
@@ -1,12 +1,11 @@
-
 CREATE TABLE IF NOT EXISTS  roles(
     Id serial NOT NULL,
     nombre varchar(100),
     descripcion varchar (250),
     fecha_creacion date default CURRENT_DATE,
     fecha_actualizacion date default CURRENT_DATE,
-     CONSTRAINT PRIMARY KEY (id)
-)
+     PRIMARY KEY (id)
+);
 
 CREATE TABLE IF NOT EXISTS  usuarios(
     id serial NOT NULL,
@@ -18,8 +17,8 @@ CREATE TABLE IF NOT EXISTS  usuarios(
     celular varchar(20),
     fecha_creacion date default CURRENT_DATE,
     fecha_actualizacion date default CURRENT_DATE,
-    CONSTRAINT PRIMARY KEY (id)
-)
+    PRIMARY KEY (id)
+);
 
 CREATE TABLE IF NOT EXISTS  compras(
     id serial NOT NULL,
@@ -29,38 +28,38 @@ CREATE TABLE IF NOT EXISTS  compras(
     forma_pago varchar,
     fecha_creacion date default CURRENT_DATE,
     fecha_actualizacion date default CURRENT_DATE,
-    CONSTRAINT PRIMARY KEY (id)
-)
+    PRIMARY KEY (id)
+);
 
 CREATE TABLE IF NOT EXISTS  curso(
     id serial,
     nombre varchar(200),
     descripcion varchar(500),
     precio decimal,
-    CONSTRAINT PRIMARY KEY (id)
-)
+    PRIMARY KEY (id)
+);
 
 CREATE TABLE IF NOT EXISTS  area(
     id serial not null,
     nombre varchar(200),
     descripcion varchar(500),
-    CONSTRAINT PRIMARY KEY (id)
-)
+    PRIMARY KEY (id)
+);
 
 CREATE TABLE IF NOT EXISTS  tema(
     id serial not null,
     nombre varchar(200),
     descripcion varchar(200),
-    CONSTRAINT PRIMARY KEY (id)
-)
+    PRIMARY KEY (id)
+);
 
 CREATE TABLE IF NOT EXISTS  respuesta(
     id serial not null,
     id_usuario integer,
     descripcion varchar(500),
     fecha date,
-    CONSTRAINT PRIMARY KEY (id)
-)
+    PRIMARY KEY (id)
+);
 
 CREATE TABLE IF NOT EXISTS  pregunta(
     id serial not null,
@@ -68,31 +67,31 @@ CREATE TABLE IF NOT EXISTS  pregunta(
     descripcion varchar(500),
     fecha date,
     id_foro integer,
-    CONSTRAINT PRIMARY KEY (id)
-)
+    PRIMARY KEY (id)
+);
 
 CREATE TABLE IF NOT EXISTS  foro(
     id serial not null,
     nombre varchar(200),
-    CONSTRAINT PRIMARY KEY (id)
-)
+    PRIMARY KEY (id)
+);
 
 CREATE TABLE IF NOT EXISTS  video(
     id serial not null,
     nombre varchar(200),
     descripcion varchar(200),
-    CONSTRAINT PRIMARY KEY (id)
-)
+    PRIMARY KEY (id)
+);
 
 CREATE TABLE IF NOT EXISTS  impartir(
     id serial not null,
     id_curso integer,
     id_profesor integer,
-    CONSTRAINT PRIMARY KEY (id)
-)
+    PRIMARY KEY (id)
+);
 
 CREATE TABLE IF NOT EXISTS  profesor(
     id serial not null,
     nombre varchar(200),
-    CONSTRAINT PRIMARY KEY (id)
-)
+    PRIMARY KEY (id)
+);
